94be355281bfe8258576727ff1f06a4a411e7e0b
Yan Duarte <yan_asd@hotmail.com>
2017-08-28 19:11:26 -0300

    Release 0.1.0.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='qStatistic',
-    version='0.1.0.1',
+    version='0.1.0.2',
     description="qStatistic Library.",
     long_description=readme + '\n\n' + history,
     author="Yan Anderson Siriano Duarte",
